921f6496666d1d5272ced8634fb505ebbb17adc8
Ahmetsargin <ahmet.sarginq@ftechlabs.com>
2024-10-10 10:56:15 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CountdownApp from './components/CountdownApp'
+
+function App() {
+    return (
+        <div className="min-h-screen bg-background text-foreground">
+            <CountdownApp />
+        </div>
+    )
+}
+
+export default App
